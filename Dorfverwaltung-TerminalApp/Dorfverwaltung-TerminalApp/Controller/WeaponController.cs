fa5d999577e7efd9a214f39cc2a8b166036c0f79
TomRR <resingtr@gmail.com>
2020-04-23 16:28:50 +0200

    view test

---
+++ Dorfverwaltung-TerminalApp/Dorfverwaltung-TerminalApp/Controller/WeaponController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dorfverwaltung_TerminalApp.Controller
+{
+    class WeaponController
+    {
+        public IKingdom Add()
+        {
+
+            Console.WriteLine("Wie heisst der Zwerg:");
+            string type = Console.ReadLine();
+            Console.WriteLine("Welchen Magischen Wert hat die Waffe");
+            int magicalValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hier kannst du eine Beschreibung hinzufuegen");
+            string description = Console.ReadLine();
+            Console.WriteLine("Hier kannst du eine Beschreibung hinzufuegen");
+            string ownerId = Console.ReadLine();
+
+            Weapon newWeapon = new Weapon()
+            {
+                WeaponType = type,
+                MagicalValue = magicalValue,
+                Description = description,
+                OwnerId = ownerId,
+            };
+            return newWeapon;
+        }
+
+        public void EnterMenu(List<IKingdom> kingdomData)
+        {
+            Console.WriteLine("Weapon");
+        }
+
+        public void PrintAll(List<IKingdom> kingdomData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowDetail(List<IKingdom> kingdomData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dorfverwaltung-TerminalApp/Dorfverwaltung-TerminalApp/View/WeaponView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dorfverwaltung_TerminalApp.View
+{
+    class WeaponView
+    {
+    }
+}
